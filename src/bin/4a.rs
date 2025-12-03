9bd18af0d86d526a698ee406c21807074d0ba82c
lulkien <kien.luuhoang.arch@gmail.com>
2025-12-03 23:39:51 +0700

    4a

---
+++ src/bin/4a.rs
@@ -0,0 +1,53 @@
+#![allow(unused)]
+
+use std::io::{self, BufRead};
+
+macro_rules! take_var {
+    ($t:ty) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .next()
+            .unwrap()
+            .parse::<$t>()
+            .unwrap()
+    }};
+}
+
+macro_rules! take_vec {
+    ($t:ty) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|s| s.parse::<$t>().unwrap())
+            .collect::<Vec<$t>>()
+    }};
+}
+
+macro_rules! take_tuple {
+    ( $( $t:ty ),+ $(,)? ) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+
+        let mut iter = line.split_whitespace();
+
+        (
+            $(
+                iter.next().unwrap().parse::<$t>().unwrap()
+            ),*
+        )
+    }};
+}
+
+fn solve() {
+    // =========================== YOUR  CODE HERE ===========================
+}
+
+fn main() {
+    let n = take_var!(usize);
+
+    println!("{}", if n > 2 && n % 2 == 0 { "YES" } else { "NO" });
+}
