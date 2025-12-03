2173d161ad70ce2bdc1aa3153c25b1781003df53
lulkien <kien.luuhoang.arch@gmail.com>
2025-12-04 00:23:21 +0700

    236a

---
+++ src/bin/236a.rs
@@ -0,0 +1,65 @@
+#![allow(unused)]
+
+use std::io::{self, BufRead};
+
+macro_rules! take_var {
+    ($t:ty) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .next()
+            .unwrap()
+            .parse::<$t>()
+            .unwrap()
+    }};
+}
+
+macro_rules! take_vec {
+    ($t:ty) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|s| s.parse::<$t>().unwrap())
+            .collect::<Vec<$t>>()
+    }};
+}
+
+macro_rules! take_tuple {
+    ( $( $t:ty ),+ $(,)? ) => {{
+        let stdin = io::stdin();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+
+        let mut iter = line.split_whitespace();
+
+        (
+            $(
+                iter.next().unwrap().parse::<$t>().unwrap()
+            ),*
+        )
+    }};
+}
+
+fn solve() {
+    // =========================== YOUR  CODE HERE ===========================
+}
+
+fn main() {
+    let name = take_var!(String);
+    let mut chars: Vec<u8> = name.chars().map(|c| c as u8).collect();
+    chars.sort_unstable();
+    chars.dedup();
+    
+    let c = chars.len();
+
+    println!(
+        "{}",
+        if c % 2 == 0 {
+            "CHAT WITH HER!"
+        } else {
+            "IGNORE HIM!"
+        }
+    )
+}
